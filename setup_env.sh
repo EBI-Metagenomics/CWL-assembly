22d367fc926741a3a49737f14ebb3dc088918b35
Miguel Boland <mdb@ebi.ac.uk>
2018-08-28 16:01:27 +0100

    More fixes for paths in install script

--- setup_env.sh
+++ setup_env.sh
@@ -6,7 +6,9 @@
 #pip install -r requirements-test.txt
 #
 
-INSTALL_DIR="$(dirname $(dirname $(which python)))"
+INSTALL_DIR="$(dirname $(dirname $(which python)))";
+CURL_VERSION="7.61.0";
+
 echo "Checking docker installation..."
 if ! [ -x "$(command -v docker)" ]; then
     echo "Docker not found, installing udocker in ${INSTALL_DIR}"
@@ -16,10 +18,11 @@ if ! [ -x "$(command -v docker)" ]; then
     echo "Installed udocker in venv"
 
     echo "Installing latest curl to bypass HTTP 400 error in udocker pull"
-    wget -O ${INSTALL_DIR}/.. https://curl.haxx.se/download/curl-7.61.0.tar.gz;
-    cd ${INSTALL_DIR}/curl-7.61.0;
+    wget -O ${INSTALL_DIR}/curl-${CURL_VERSION}.tar.gz https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz;
+    tar -xzf ${INSTALL_DIR}/curl-${CURL_VERSION}.tar.gz
+    cd ${INSTALL_DIR}/curl-${CURL_VERSION};
     make;
-    mv ${INSTALL_DIR}/src/curl ${INSTALL_DIR}/venv/bin/;
+    mv ${INSTALL_DIR}/curl-${CURL_VERSION}/src/curl ${INSTALL_DIR}/venv/bin/;
     cd ${INSTALL_DIR};
     echo "Installed curl v7.61 in venv"
 else
@@ -30,7 +33,7 @@ echo "Checking for node installation..."
 if ! [ -x "$(command -v node)" ]; then
     echo "Node not found, installing in ${INSTALL_DIR}"
     curl https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-x64.tar.xz > ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz
-    tar -xJf ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz -C ${INSTALL_DIR}
+    tar -xzf ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz -C ${INSTALL_DIR}
     export PATH="${INSTALL_DIR}/node-v8.11.1-linux-x64/bin/:$PATH"
     echo "PATH=${INSTALL_DIR}/node-v8.11.1-linux-x64/bin/:\$PATH" >> ${INSTALL_DIR}/bin/activate
     echo "Installed node in venv"
