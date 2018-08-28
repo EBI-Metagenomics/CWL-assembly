f15a6f89fbf82ee8efa49cf3d804efef376f9d23
Miguel Boland <mdb@ebi.ac.uk>
2018-08-28 16:32:24 +0100

    Cleaned up setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #virtualenv venv
 #source venv/bin/activate
 #pip install -U pip setuptools
@@ -11,30 +11,30 @@ CURL_VERSION="7.61.0";
 
 echo "Checking docker installation..."
 if ! [ -x "$(command -v docker)" ]; then
-    echo "Docker not found, installing udocker in ${INSTALL_DIR}"
-    curl https://raw.githubusercontent.com/indigo-dc/udocker/devel/udocker.py > ${INSTALL_DIR}/bin/udocker
-    chmod 775 ${INSTALL_DIR}/bin/udocker
-    udocker install
-    echo "Installed udocker in venv"
+    echo "Docker not found, installing udocker in ${INSTALL_DIR}";
+    curl https://raw.githubusercontent.com/indigo-dc/udocker/devel/udocker.py > ${INSTALL_DIR}/bin/udocker;
+    chmod 775 ${INSTALL_DIR}/bin/udocker;
+    udocker install;
+    echo "Installed udocker in venv";
 
-    echo "Installing latest curl to bypass HTTP 400 error in udocker pull"
+    echo "Installing latest curl to bypass HTTP 400 error in udocker pull";
     wget -O ${INSTALL_DIR}/curl-${CURL_VERSION}.tar.gz https://curl.haxx.se/download/curl-${CURL_VERSION}.tar.gz;
     tar -xzf ${INSTALL_DIR}/curl-${CURL_VERSION}.tar.gz
     cd ${INSTALL_DIR}/curl-${CURL_VERSION};
     make;
     mv ${INSTALL_DIR}/curl-${CURL_VERSION}/src/curl ${INSTALL_DIR}/venv/bin/;
     cd ${INSTALL_DIR};
-    echo "Installed curl v7.61 in venv"
+    echo "Installed curl v7.61 in venv";
 else
-    echo "Found docker installation."
+    echo "Found docker installation.";
 fi
 
-echo "Checking for node installation..."
+echo "Checking for node installation...";
 if ! [ -x "$(command -v node)" ]; then
-    echo "Node not found, installing in ${INSTALL_DIR}"
-    curl https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-x64.tar.xz > ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz
-    tar -xzf ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz -C ${INSTALL_DIR}
-    export PATH="${INSTALL_DIR}/node-v8.11.1-linux-x64/bin/:$PATH"
+    echo "Node not found, installing in ${INSTALL_DIR}";
+    curl https://nodejs.org/dist/v8.11.1/node-v8.11.1-linux-x64.tar.xz > ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz;
+    tar -xzf ${INSTALL_DIR}/node-v8.11.1-linux-x64.tar.xz -C ${INSTALL_DIR};
+    export PATH="${INSTALL_DIR}/node-v8.11.1-linux-x64/bin/:$PATH";
     echo "PATH=${INSTALL_DIR}/node-v8.11.1-linux-x64/bin/:\$PATH" >> ${INSTALL_DIR}/bin/activate
     echo "Installed node in venv"
 else
